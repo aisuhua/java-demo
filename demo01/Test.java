4d551f7d5212438b5c6a80d0c621aa14898f1d89
aisuhua <1079087531@qq.com>
2024-04-30 19:37:32 +0800

    init

---
+++ demo01/Test.java
@@ -0,0 +1,154 @@
+public class Test {
+    public static void main(String[] args) {
+//        int x = 1;
+//        System.out.println(x);
+//
+//        int y = x;
+//        System.out.println(y);
+//
+//        System.out.println(++x);
+//
+//        long z = 100L;
+//        System.out.println(z);
+//
+//        float f1 = 3.14f;
+//        System.out.println(f1);
+//
+//        double d1 = 3.14;
+//        System.out.println(d1);
+//
+//        System.out.println(1.51e100);
+//
+//        boolean b1 = true;
+//        boolean b2 = false;
+//        System.out.println(b1);
+//        System.out.println(b2);
+//
+//        char c1 = 'a';
+//        char c2 = '中';
+//        System.out.println(c1);
+//        System.out.println(c2);
+//
+//        String s = "Hello World";
+//        System.out.println(s);
+//
+//        String s1 = s;
+//        System.out.println(s1);
+//        s1 = "Hello";
+//        System.out.println(s1);
+//        System.out.println(s);
+//
+//        String s2 = "Hello World";
+//        System.out.println(s2 == s);
+//
+//        final double PI = 3.14;
+//        System.out.println(PI);
+//        PI = 3.14159;
+//        System.out.println(PI);
+
+//        var x = 100;
+//        System.out.println(x);
+//
+//        var y = "Hello World";
+//        System.out.println(y);
+//
+//        StringBuilder a = new StringBuilder();
+//        a.append("Hello World");
+//        System.out.println(a);
+//
+//        var b = new StringBuilder();
+//        b.append("Hello World");
+//        System.out.println(b);
+//
+//        System.out.println(a == b);
+
+//        int i = (100 + 200) * 10 / 20;
+//        System.out.println(i);
+//
+//        System.out.println(100 / 9);
+//        System.out.println(100 % 9);
+
+//        int x = 100;
+//        short y = 10;
+//        int z = x + y;
+//        System.out.println(z);
+//
+//        short s = (short)(x);
+//        System.out.println(s);
+
+//        double d1 = 0.0 / 0;
+//        System.out.println(d1);
+//        double d2 = 1.0 / 0;
+//        System.out.println(d2);
+//        double d3 = -1.0 / 0;
+//        System.out.println(d3);
+
+//        int x = (int) 1.1;
+//        int y = (int) 1.5;
+//        int z = (int) -1.9;
+//        System.out.println(x);
+//        System.out.println(y);
+//        System.out.println(z);
+
+//        double d = 2.1;
+//        int n = (int) (d + 0.5);
+//        System.out.println(n);
+
+//        System.out.println(1 > 2);
+//        System.out.println(1 > 2 ? 1 : 2);
+
+//        char c1 = 'a';
+//        char c2 = '中';
+//
+//        int x = c1;
+//        int y = c2;
+//        System.out.println(c1);
+//        System.out.println(c2);
+//        System.out.println(x);
+//        System.out.println(y);
+
+//        String s = "abc\n111";
+//        System.out.println(s);
+//
+//        String s1 = "Hello";
+//        String s2 = "World";
+//        System.out.println(s1 + " " + s2 + " " + 123);
+
+//        String s = "Hello \n"
+//                + "second Hello \n"
+//                + "third hello \n";
+//        System.out.println(s);
+//
+//        String s2 = """
+//                Hello
+//                    second Hello
+//                third Hello""";
+//        System.out.println(s2);
+
+//        String s1 = "Hello";
+//        String t1 = s1;
+//        s1 = "World";
+//        System.out.println(t1);
+//        System.out.println(s1);
+
+//        int[] a = new int[5];
+//        a[0] = 1;
+//        a[1] = 2;
+//        a[2] = 3;
+//        a[3] = 4;
+//        a[4] = 5;
+//        System.out.println(a[0]);
+//        System.out.println(a.length);
+//        System.out.println(a[5]);
+
+//        int[] ns = new int[] {1, 2, 3, 4, 5};
+//        int[] ns = {1, 2, 3, 4, 5};
+//        System.out.println(ns.length);
+
+//        String[] ns = { "Hello", "World", "OK" };
+//        String s = ns[1];
+//        ns[1] = "abc";
+//        System.out.println(s);
+
+    }
+}
