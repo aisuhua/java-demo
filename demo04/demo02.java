8d0b8e0ab5616a3bd41eb6044c62ea6f4507d2e8
aisuhua <1079087531@qq.com>
2024-05-28 12:50:01 +0800

    demo02.java

--- demo04/demo02.java
+++ demo04/demo02.java
@@ -16,7 +16,7 @@ public class demo01 {
 //
 //        System.out.println(str1.equalsIgnoreCase(str3));
 
-        String str = "suhua is a good boy!";
+        String str = " suhua is a good boy! \n";
         System.out.println(str.contains("!"));
 
         System.out.println(str.indexOf("!"));
@@ -26,5 +26,12 @@ public class demo01 {
         System.out.println(str.startsWith("suhua"));
         System.out.println(str.endsWith("is"));
         System.out.println(str.substring(2, 5));
+        System.out.println(str);
+        System.out.println(str.trim());
+
+        System.out.println(" ".isEmpty());
+        System.out.println(" \n ".isBlank());
+
+        System.out.println(str.replace("suhua", "baidu"));
     }
 }
