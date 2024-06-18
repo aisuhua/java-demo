f6d366d48fcbcb8e07b936b33691a17d3be4ff34
aisuhua <1079087531@qq.com>
2024-06-18 18:20:01 +0800

    demo03.java

--- demo04/demo03.java
+++ demo04/demo03.java
@@ -1,5 +1,23 @@
 public class demo03 {
     public static void main(String[] args) {
-        String str = "suhua is a good boy.";
+//        String str = "suhua is a good boy.";
+//        System.out.println(str.split(" "));
+//
+//        for (String value : str.split(" ")) {
+//            System.out.println(value);
+//        }
+//
+//        String arr[] = str.split(" ");
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(arr[i]);
+//        }
+
+        String arr[] = {"A", "B", "C"};
+        System.out.println(String.join(",", arr));
+
+        String str = "%s is a good boy, he is %d years old.";
+        System.out.println(str.formatted("Suhua", Integer.parseInt("100")));
+
+        System.out.println(String.format(str, "Suhua", 100));
     }
 }
