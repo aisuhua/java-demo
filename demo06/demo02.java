78bd069cc900126c5f24bd3ef3539be892aed90d
aisuhua <1079087531@qq.com>
2024-06-21 22:30:02 +0800

    demo01.java

---
+++ demo06/demo02.java
@@ -0,0 +1,11 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+public class demo {
+    public static void main(String[] args) {
+        Log log = LogFactory.getLog(demo.class);
+        log.info("info");
+        log.warn("warn");
+    }
+}
