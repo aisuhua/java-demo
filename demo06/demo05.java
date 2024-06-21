c4af5b1aee0e104cce0b33a1076fb6916860a4b3
aisuhua <1079087531@qq.com>
2024-06-21 23:40:01 +0800

    demo05.java log4j-api-2.23.1.jar log4j-core-2.23.1.jar log4j-jcl-2.23.1.jar log4j2.xml

--- demo06/demo05.java
+++
@@ -1,9 +0,0 @@
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-public class demo {
-    public static void main(String[] args) {
-        Log log = LogFactory.getLog(demo.class);
-        log.info("suhua is a good boy.");
-    }
-}
