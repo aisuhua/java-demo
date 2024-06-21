94bf266d83df03287947a6b65f395940e0efb92a
aisuhua <1079087531@qq.com>
2024-06-21 22:40:01 +0800

    commons-logging-1.3.2.jar demo02.java demo03.java demo04.java

--- demo06/demo04.java
+++ demo06/demo04.java
@@ -2,9 +2,23 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class demo {
-    protected static final Log log = LogFactory.getLog(demo.class);
-
     public static void main(String[] args) {
-        log.info("info");
+        B b = new B();
+        b.print();
+        b.add();
+    }
+}
+
+class A {
+    protected final Log log = LogFactory.getLog(getClass());
+}
+
+class B extends A {
+    public void print() {
+        this.log.info("suhua");
+    }
+
+    public void add() {
+        this.print();
     }
 }
