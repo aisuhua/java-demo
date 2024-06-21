cd23fbacae7294d125693567e61e38adf238b973
aisuhua <1079087531@qq.com>
2024-06-21 23:30:02 +0800

    demo04.java

---
+++ demo06/demo05.java
@@ -0,0 +1,9 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class demo {
+    public static void main(String[] args) {
+        Log log = LogFactory.getLog(demo.class);
+        log.info("suhua is a good boy.");
+    }
+}
