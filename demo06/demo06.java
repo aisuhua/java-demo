90f4a6aed9d79403ba99b7e2989080a734505a27
aisuhua <1079087531@qq.com>
2024-06-23 19:40:01 +0800

    demo04.java demo05.class demo05.java demo06.java err.log logback-classic-1.5.6.jar logback-core-1.5.6.jar slf4j-api-1.7.9.jar

--- demo06/demo06.java
+++ demo06/demo06.java
@@ -1,8 +1,10 @@
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
-public class demo05 {
+public class demo06 {
     public static void main(String[] args) {
-
+        Logger log = LoggerFactory.getLogger(demo06.class);
+        log.info("suhua is a good boy.");
+        log.error("suhua is a good boy...");
     }
 }
