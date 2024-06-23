715f1f814492c31e93ed744517d1176c9ad83b7e
aisuhua <1079087531@qq.com>
2024-06-23 19:50:01 +0800

    demo06.class demo06.java output.log logback-classic-1.2.9.jar logback-classic-1.5.6.jar logback-core-1.2.9.jar logback-core-1.5.6.jar logback.xml

--- demo06/demo06.java
+++ demo06/demo06.java
@@ -1,6 +1,12 @@
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
+/**
+ * java -cp ./slf4j-api-1.7.9.jar:./logback-core-1.2.9.jar:./logback-classic-1.2.9.jar demo06.java
+ * 或者
+ * javac -cp ./slf4j-api-1.7.9.jar:./logback-core-1.2.9.jar:./logback-classic-1.2.9.jar demo06.java
+ * java -cp .:./slf4j-api-1.7.9.jar:./logback-core-1.2.9.jar:./logback-classic-1.2.9.jar demo06
+ */
 public class demo06 {
     public static void main(String[] args) {
         Logger log = LoggerFactory.getLogger(demo06.class);
