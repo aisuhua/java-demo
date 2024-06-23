6d377281750b41eab3187b0d9a61b415a55e8eb9
aisuhua <1079087531@qq.com>
2024-06-23 20:20:02 +0800

    demo01.java

--- demo07/demo01.java
+++ demo07/demo01.java
@@ -1,6 +1,14 @@
 class demo01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         Class cls = String.class;
         System.out.println(cls);
+
+        String str = new String("hello");
+        System.out.println(str.getClass());
+
+        Class cls2 = Class.forName("java.lang.String");
+        System.out.println(cls2);
+
+        System.out.println(cls == cls2);
     }
 }
