54fa16a30fdf7c4f7485cf5128e81fa17fc574ac
aisuhua <1079087531@qq.com>
2024-07-01 12:50:01 +0800

    demo02.java demo01.java

--- demo09/demo01.java
+++ demo09/demo01.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+
 public class demo {
     public static void main(String[] args) {
-        
+        ArrayList alist = new ArrayList();
+        alist.add("suhua");
+        alist.add(111);
+
+        System.out.println(alist.get(0));
+        System.out.println(alist.get(1));
+
+        String str = (String) alist.get(0);
+        System.out.println(str.length());
     }
 }
---
+++ demo09/demo02.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class demo {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add("abc");
+
+    }
+}
