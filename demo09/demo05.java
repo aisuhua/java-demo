e1e4252432321282207d5f0f4edac43b95e7e4fb
aisuhua <1079087531@qq.com>
2024-07-04 12:20:01 +0800

    demo04.java

---
+++ demo09/demo05.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class demo {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Pair<T> {
+    public T first;
+    public T last;
+
+    public Pair(T first, T last) {
+        this.first = first;
+        this.last = last;
+    }
+}
