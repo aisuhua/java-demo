70b629c5ff59bffc08814368688ea1404c4cf582
aisuhua <1079087531@qq.com>
2024-07-07 10:40:01 +0800

    demo05.java

---
+++ demo10/demo06.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Arrays;
+
+class demo {
+    public static void main(String[] args) {
+
+        // List list = new ArrayList();
+        // List<String> list = new ArrayList<String>();
+//        List<String> list = new ArrayList<>();
+//        list.add("A");
+//        list.add("B");
+//        System.out.println(list);
+        List<String> list = List.of("A", "B");
+        System.out.println(list);
+
+    }
+}
