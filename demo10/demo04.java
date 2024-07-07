264f695003fed6e036e20d1cd72e6308395f7d09
aisuhua <1079087531@qq.com>
2024-07-07 10:20:01 +0800

    demo03.java demo04.java

--- demo10/demo04.java
+++ demo10/demo04.java
@@ -10,6 +10,7 @@ class demo {
         System.out.println(list.getClass());
 
         // list 转 array
+        // 方式1：会丢失类型信息
         Object[] arr = list.toArray();
         System.out.println(arr);
         System.out.println(Arrays.toString(arr));
@@ -18,8 +19,24 @@ class demo {
             System.out.println(o.getClass());
         }
 
+        // 方式2：使用带类型的数组让 list 填充
         Integer[] arr2 = list.toArray(new Integer[list.size()]);
         System.out.println(Arrays.toString(arr2));
         System.out.println(arr2);
+
+        // 方式3：使用函数式写法 new Integer[]::new
+        Integer[] arr3 = list.toArray(Integer[]::new);
+        System.out.println(Arrays.toString(arr3));
+        System.out.println(arr3);
+
+        // array 转 list
+        Integer[] arr4 = {1, 2, 3};
+        List list2 = List.of(arr4);
+        System.out.println(list2);
+        System.out.println(list2.getClass());
+
+        // Error
+        ArrayList<Integer> list3 = List.of(arr4);
+        System.out.println(list3);
     }
 }
